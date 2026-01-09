ad3327642478bdcdea6117b30d8d1ebc144da55a
renegadec <confidencenyirenda7@gmail.com>
2026-01-09 13:34:01 +0200

    added footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home'
 import CarDetails from './pages/CarDetails'
 import Cars from './pages/Cars'
 import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
 
 const App = () => {
 
@@ -23,6 +24,8 @@ const App = () => {
         <Route path='/cars'element={<Cars />} />
         <Route path='/my-bookings' element={<MyBookings />} />
       </Routes>
+
+      {!isOwnerPath && <Footer />}
     </>
   )
 }
